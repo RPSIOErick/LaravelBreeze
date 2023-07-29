a366ea08e27f1c4e03704b8546c1a2ccbcd84c8a
RPSIOErick <ecerickbastos@gmail.com>
2023-07-29 02:33:43 -0300

    Projeto Laravel Breeze

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="/">
-                        <img src="https://eteczonaleste.com.br/img/logo-etec.png" id="img-logo">
+                        <x-application-logo class="block h-9 w-auto fill-current text-black-800"/>
                     </a>
                 </div>
 
